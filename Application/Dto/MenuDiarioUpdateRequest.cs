47f6b2e153b6b0e08c7ad754efccc6afa2d73f3f
Luis Valeriano <luisvaleriano1009@gmail.com>
2026-05-07 18:59:55 -0500

    Fix 2 update menu

--- Application/Dto/MenuDiarioUpdateRequest.cs
+++ Application/Dto/MenuDiarioUpdateRequest.cs
@@ -3,7 +3,6 @@ namespace MenuSoda.Application.Dto;
 public class MenuDiarioUpdateRequest
 {
     public int Id { get; set; }
-    public DateTime Fecha { get; set; }
     public int EstadoId { get; set; }
     public List<int> EntradasIds { get; set; } = new();
     public List<MenuDiarioPlatoRequest> Platos { get; set; } = new();
