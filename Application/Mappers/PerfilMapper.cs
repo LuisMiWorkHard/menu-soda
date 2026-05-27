677937c4af1951389e23029e819e62470b423638
Luis Valeriano <luisvaleriano1009@gmail.com>
2026-05-26 23:05:12 -0500

    Avance Tab Perfil en Mapper

--- Application/Mappers/PerfilMapper.cs
+++ Application/Mappers/PerfilMapper.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using MenuSoda.Application.Dto;
 using MenuSoda.Domain.Entities;
 
@@ -5,12 +6,17 @@ namespace MenuSoda.Application.Mappers;
 
 public static class PerfilMapper
 {
-    public static PerfilResponse ToResponse(this User entity) =>
-        new()
+    public static PerfilResponse ToResponse(this User entity)
+    {
+        var nombreCompleto = string.Join(" ", new[] { entity.Usunom, entity.Usuapepat, entity.Usuapemat }
+            .Where(s => !string.IsNullOrWhiteSpace(s)));
+
+        return new PerfilResponse
         {
-            NombreCompleto = string.Join(" ", new[] { entity.Usunom, entity.Usuapepat, entity.Usuapemat }
-                .Where(s => !string.IsNullOrWhiteSpace(s))).Trim(),
-            Email = entity.Usuemail,
+            NombreCompleto = CultureInfo.InvariantCulture.TextInfo
+                .ToTitleCase(nombreCompleto.Trim().ToLowerInvariant()),
+            Email = entity.Usuemail.Trim().ToLowerInvariant(),
             Telefono = entity.Usutel
         };
+    }
 }
