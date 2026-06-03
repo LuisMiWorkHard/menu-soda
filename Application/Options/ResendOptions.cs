81e457239f6d735e9dec24727dcb1848974ab174
Luis Valeriano <luisvaleriano1009@gmail.com>
2026-06-02 22:26:49 -0500

    Modificacion Api Resend

--- Application/Options/ResendOptions.cs
+++ Application/Options/ResendOptions.cs
@@ -2,8 +2,7 @@ namespace MenuSoda.Application.Options;
 
 public class ResendOptions
 {
-    public string ApiKey           { get; set; } = "";
-    public string FromEmail        { get; set; } = "";
+    public string FromEmail        { get; set; } = "onboarding@resend.dev";
     public string FromName         { get; set; } = "MenuSoda";
     public string ResetPasswordUrl { get; set; } = "";
     public int    MaxReenvios      { get; set; } = 3;
